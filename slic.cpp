e40dab2fa430400f8a6d05aaebb550d52597e2c3
berak <px1704@web.de>
2014-12-23 18:53:31 +0100

    Scalar trouble on 2.4.9

--- slic.cpp
+++ slic.cpp
@@ -282,7 +282,7 @@ void Slic::create_connectivity(const cv::Mat &image) {
  * Input : The image to display upon (cv::Mat) and the colour (cv::Vec3b).
  * Output: -
  */
-void Slic::display_center_grid(cv::Mat &image, cv::Vec3b colour) {
+void Slic::display_center_grid(cv::Mat &image, cv::Scalar colour) {
     for (int i = 0; i < (int) centers.size(); i++) {
         cv::circle(image, cv::Point2d(centers[i][3], centers[i][4]), 2, colour, 2);
     }
